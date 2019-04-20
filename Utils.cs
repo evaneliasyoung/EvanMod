f922924666bbe800623ed16f6dc93ea92d213ea2
DocCodes <evanyoung762@gmail.com>
2019-04-20 15:11:10 -0500

    :ok_hand: Removed unused directive

--- Utils.cs
+++ Utils.cs
@@ -4,12 +4,11 @@
 *
 *  @author    Evan Elias Young
 *  @date      2019-04-16
-*  @date      2019-04-16
+*  @date      2019-04-20
 *  @copyright Copyright 2017-2019 Evan Elias Young. All rights reserved.
 */
 
 using Microsoft.Xna.Framework;
-using System;
 
 namespace EvanModpack
 {
