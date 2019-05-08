0cb5435b90b6f560258cb9a4d367e811c63ef88c
DocCodes <evanyoung762@gmail.com>
2019-05-08 15:08:19 -0500

    :bug: Fixed Slag Generation

--- NPCs/NPCOverrides.cs
+++ NPCs/NPCOverrides.cs
@@ -4,7 +4,7 @@
 *
 *  @author    Evan Elias Young
 *  @date      2017-07-20
-*  @date      2019-04-28
+*  @date      2019-05-08
 *  @copyright Copyright 2017-2019 Evan Elias Young. All rights reserved.
 */
 
@@ -34,7 +34,7 @@ namespace EvanModpack.NPCs
 		public void GenerateSlag()
 		{
 			int slagVeins = 45;
-			int slagLoop = (int)Math.Round((float)(Main.maxTilesY * slagVeins) / 6);
+			int slagLoop = (int)Math.Round((float)(Main.maxTilesY * slagVeins) / 9);
 			Main.NewText(Language.GetTextValue("Mods.EvanModpack.Misc.SlagEnter"), Utils.ChatColors.Info);
 
 			for (int i = 0; i < slagLoop; ++i)
