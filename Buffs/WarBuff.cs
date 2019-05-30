685ced2b9213ddc087ab35367faad844be3c140c
evaneliasyoung <evaneliasyoung@users.noreply.github.com>
2019-05-30 15:20:14 -0500

    :bug: Fixed potion timing

--- Buffs/WarBuff.cs
+++ Buffs/WarBuff.cs
@@ -4,7 +4,7 @@
 *
 *  @author    Evan Elias Young
 *  @date      2017-04-22
-*  @date      2019-04-16
+*  @date      2019-05-30
 *  @copyright Copyright 2017-2019 Evan Elias Young. All rights reserved.
 */
 
@@ -27,12 +27,6 @@ namespace EvanModpack.Buffs
 			Main.buffNoTimeDisplay[Type] = false;
 			Main.debuff[Type] = false;
 			Main.pvpBuff[Type] = false;
-		}
-
-		public override void Update(Player player, ref int buffIndex)
-		{
-			player.buffTime[buffIndex] = 420;
-			base.Update(player, ref buffIndex);
 		}
 
 		private class SpawnRateMultiplierGlobalNPC : GlobalNPC
