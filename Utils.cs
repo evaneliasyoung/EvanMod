117ca7a6bc6de12be296358084a1c7f43d573ef8
Evan Elias Young <me@eyoung.dev>
2024-03-28 22:31:58 -0500

    revert: :fire: remove most features
    Removes items and features which are now in the base game, broken features, and out-of-scope features.

--- Utils.cs
+++ Utils.cs
@@ -8,33 +8,19 @@
 *  @copyright Copyright 2017-2019 Evan Elias Young. All rights reserved.
 */
 
-using Microsoft.Xna.Framework;
-
 namespace EvanModpack
 {
-	internal static class Utils
-	{
-		// Global Public Variables
-		public static Color OriginalSlagColor = new Color(157, 56, 56);
-		public static Color SlagColor = new Color(120, 30, 30);
-		public static int MapRevealSize = 255;
+    internal static class Utils
+    {
 
-		internal class ChatColors
-		{
-			public static Color Info = Color.SpringGreen;
-			public static Color Boss = Color.DarkOrchid;
-			public static Color Party = Color.MediumVioletRed;
-			public static Color Death = Color.Crimson;
-		}
+        public static int FrameTime(int seconds)
+        {
+            return seconds * 60;
+        }
 
-		public static int FrameTime(int seconds)
-		{
-			return seconds * 60;
-		}
-
-		public static int FrameTime(int minutes, int seconds)
-		{
-			return FrameTime(minutes * 60 + seconds);
-		}
-	}
+        public static int FrameTime(int minutes, int seconds)
+        {
+            return FrameTime(minutes * 60 + seconds);
+        }
+    }
 }
