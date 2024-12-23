6311bd5a46d7108abdabc81f0a59fd045040118a
Evan Elias Young <evanyoung762@gmail.com>
2024-12-23 03:40:12 -0600

    feat: :sparkles: add HookStatsAndWingStats support

---
+++ Common/Systems/Crossover.cs
@@ -0,0 +1,27 @@
+using EvanMod.Content.Items.Tools;
+using Terraria.ModLoader;
+
+namespace EvanMod.Common.Systems
+{
+    public class Crossover : ModSystem
+    {
+        private static void SetupHookStatsAndWingStats()
+        {
+            var stats = new HookStatsAndWingStats.Helpers.HookStats(
+                GemstoneHook.RANGE_IN_TILES * 16f,
+                GemstoneHook.LAUNCH_SPEED,
+                GemstoneHook.TOTAL_HOOKS,
+                HookStatsAndWingStats.Helpers.Enums.HookLatchingType.Simultaneous
+            );
+            HookStatsAndWingStats.Common.Systems.HookSystem.HookStats.TryAdd("EvanMod:GemstoneHook", stats);
+        }
+
+        public override void PostSetupContent()
+        {
+            if (ModLoader.HasMod("HookStatsAndWingStats"))
+            {
+                SetupHookStatsAndWingStats();
+            }
+        }
+    }
+}
