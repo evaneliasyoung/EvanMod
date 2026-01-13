ad3d7f53b43de281d3c89e421014e028ab887327
Evan Elias Young <evanyoung762@gmail.com>
2026-01-13 16:09:50 -0600

    feat: :sparkles: add coin rolls

---
+++ Content/Items/Typeless/GoldCoinRoll.cs
@@ -0,0 +1,31 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EvanMod.Content.Items.Typeless
+{
+    class GoldCoinRoll : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 28;
+            Item.value = 0;
+            Item.maxStack = 9999;
+            Item.rare = ItemRarityID.White;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.GoldCoin, 100)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+    }
+}
